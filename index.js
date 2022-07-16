29da3db5408fbbfb7eb22397c7bf56f4bc9eaab5
Zak <zakcannon12@gmail.com>
2022-07-16 13:33:32 +0100

    Simple calculator using if/else

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
+console.log('Welcome to the calculator!\n');
 
-console.log('Enter first value to be multiplied: ');
-const response = readline.prompt();
+console.log('Enter operation (+, -, /, or *): ');
+const operator = readline.prompt();
+
+console.log('Enter first value: ');
+const response1 = +readline.prompt();
 console.log('Enter second value: ');
-const response2 = readline.prompt();
+const response2 = +readline.prompt();
 
-var product = response * response2
-console.log('Product is ' + product)
+var result;
+
+if (operator == "+") {
+    result = response1 + response2;
+} else if (operator == "-") {
+    result = response1 - response2;
+} else if (operator == "*") {
+    result = response1 * response2;
+} else if (operator == "/") {
+    result = response1 / response2;
+} else {
+    result = "not a valid operation"
+}
+
+console.log(result)
