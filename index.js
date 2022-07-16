895e2c4c9de08117a644d5ce3554f166a1504510
Zak <zakcannon12@gmail.com>
2022-07-16 12:05:02 +0100

    Added simple multiplier

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
-var list1 = ['abc', 'def', 'ghi']
-console.log(list1[1])
+
+console.log('Enter first value to be multiplied: ');
+const response = readline.prompt();
+console.log('Enter second value: ');
+const response2 = readline.prompt();
+
+var product = response * response2
+console.log('Product is ' + product)
