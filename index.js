05b64dc105160b44886616cb56c358be1eb26817
Zak <zakcannon12@gmail.com>
2022-07-25 16:44:40 +0100

    tidied up code into functions

--- index.js
+++ index.js
@@ -1,42 +1,71 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!\n');
-
-console.log('Enter operation (+, -, /, or *): ');
-const operator = readline.prompt();
-
-console.log('How many numbers do you want to ' + operator + '? ');
-const n_inputs = +readline.prompt();
-var inputs = new Array(n_inputs);
-
-var number
-
-for (let i=0; i < inputs.length; i++) {
-    console.log('Enter number ' + (i+1) + ': ');
-    number = +readline.prompt();
-    inputs[i] = number
+function readNumber(message) {
+    console.log(message);
+    var inputnumber = +readline.prompt()
+    if (isNaN(inputnumber)) {
+        console.log("Response \"" + inputnumber + "\" is not a number");
+        readNumber(message);
+    } else {
+        return inputnumber;
+    }
 }
 
-var result = inputs[0]
-
-if (operator == "+") {
-    for (let i=1; i < inputs.length; i++) {
-        result += inputs[i];
-    }
-} else if (operator == "-") {
-    for (let i=1; i < inputs.length; i++) {
-        result = result - inputs[i];
-    }
-} else if (operator == "*") {
-    for (let i=1; i < inputs.length; i++) {
-        result = result * inputs[i];
-    }
-} else if (operator == "/") {
-    for (let i=1; i < inputs.length; i++) {
-        result = result / inputs[i];
-    }
-} else {
-    console.log("Not a valid operator")
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator!\n');
 }
 
-console.log(result)
+function performOneCalculation() {
+    console.log('Enter operation (+, -, /, or *): ');
+    const operator = readline.prompt();
+
+    response1 = readNumber('Enter first value: ');
+    response2 = readNumber('Enter second value: ');
+
+    var result;
+
+    switch (operator) {
+        case "+":
+            result = response1 + response2;
+            break;
+        case "-":
+            result = response1 - response2;
+            break;
+        case "*":
+            result = response1 * response2;
+            break;
+        case "/":
+            result = response1 / response2;
+            break;
+        default:
+            result = "not a valid operation"
+    }
+
+    console.log(result)
+    return result
+}
+
+function loopAgain() {
+    console.log("Perform another calculation? Y/N: ")
+    another = readline.prompt()
+    switch (another) {
+        case "Y":
+            break
+        case "N":
+            x = false
+            break
+        default:
+            console.log("Not a valid answer")
+            loopAgain()
+    }
+}
+
+var x = true;
+
+printWelcomeMessage()
+
+while (x) {
+    performOneCalculation()
+    loopAgain()
+}
+    
