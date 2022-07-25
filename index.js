fbf528c091000007deb01d03def83050301cc942
Zak <zakcannon12@gmail.com>
2022-07-25 15:30:12 +0100

    added looping over operations

--- index.js
+++ index.js
@@ -5,28 +5,38 @@ console.log('Welcome to the calculator!\n');
 console.log('Enter operation (+, -, /, or *): ');
 const operator = readline.prompt();
 
-console.log('Enter first value: ');
-const response1 = +readline.prompt();
-console.log('Enter second value: ');
-const response2 = +readline.prompt();
+console.log('How many numbers do you want to ' + operator + '? ');
+const n_inputs = +readline.prompt();
+var inputs = new Array(n_inputs);
 
-var result;
+var number
 
-switch (operator) {
-    case "+":
-        result = response1 + response2;
-        break;
-    case "-":
-        result = response1 - response2;
-        break;
-    case "*":
-        result = response1 * response2;
-        break;
-    case "/":
-        result = response1 / response2;
-        break;
-    default:
-        result = "not a valid operation"
+for (let i=0; i < inputs.length; i++) {
+    console.log('Enter number ' + (i+1) + ': ');
+    number = +readline.prompt();
+    inputs[i] = number
+}
+
+var result = inputs[0]
+
+if (operator == "+") {
+    for (let i=1; i < inputs.length; i++) {
+        result += inputs[i];
+    }
+} else if (operator == "-") {
+    for (let i=1; i < inputs.length; i++) {
+        result = result - inputs[i];
+    }
+} else if (operator == "*") {
+    for (let i=1; i < inputs.length; i++) {
+        result = result * inputs[i];
+    }
+} else if (operator == "/") {
+    for (let i=1; i < inputs.length; i++) {
+        result = result / inputs[i];
+    }
+} else {
+    console.log("Not a valid operator")
 }
 
 console.log(result)
