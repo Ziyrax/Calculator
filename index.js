5074bae0e626bafa67bb79fe0f7741026720cff5
Zak <zakcannon12@gmail.com>
2022-07-25 22:05:27 +0100

    added vowel counting functionality

--- index.js
+++ index.js
@@ -41,31 +41,92 @@ function performOneCalculation() {
             result = "not a valid operation"
     }
 
-    console.log(result)
-    return result
+    console.log(result);
+    return result;
+}
+
+function performOneVowelCount(inputstring) {
+    inputstring = inputstring.toLowerCase();
+
+    const vowelcounts = {
+        numA: 0,
+        numE: 0,
+        numI: 0,
+        numO: 0,
+        numU: 0
+    };
+
+    const vowellist = ['a', 'e', 'i', 'o', 'u']
+
+    for (let i = 0; i < inputstring.length; i++) {
+        if (vowellist.includes(inputstring[i])) {
+            switch (inputstring[i]) {
+                case "a":
+                    vowelcounts.numA += 1;
+                    break;
+                case "e":
+                    vowelcounts.numE += 1;
+                    break;
+                case "i":
+                    vowelcounts.numI += 1;
+                    break;
+                case "o":
+                    vowelcounts.numO += 1;
+                    break;
+                case "u":
+                    vowelcounts.numU += 1;
+                    break;
+            }
+        }
+    }
+
+    return vowelcounts
 }
 
 function loopAgain() {
-    console.log("Perform another calculation? Y/N: ")
-    another = readline.prompt()
+    console.log("Perform another operation? Y/N: ");
+    another = readline.prompt().toUpperCase();
     switch (another) {
         case "Y":
-            break
+            break;
         case "N":
-            x = false
-            break
+            x = false;
+            break;
         default:
-            console.log("Not a valid answer")
-            loopAgain()
+            console.log("Not a valid answer");
+            loopAgain();
+    }
+}
+
+function selectFunction() {
+    console.log("Perform vowel count or arithmetic calculation? V/A: ")
+    userselection = readline.prompt().toUpperCase()
+    switch (userselection) {
+        case "V":
+            return userselection;
+        case "A":
+            return userselection;
+        default:
+            console.log("Not a valid answer");
+            selectFunction();
     }
 }
 
 var x = true;
 
-printWelcomeMessage()
+printWelcomeMessage();
 
 while (x) {
-    performOneCalculation()
-    loopAgain()
+    choice = selectFunction();
+    if (choice == "V") {
+        console.log("Enter string to vowel count: ");
+        userstring = readline.prompt();
+
+        counts = performOneVowelCount(userstring);
+        console.log(counts);
+    } else if (choice == "A") {
+        performOneCalculation();
+    }
+    loopAgain();
 }
     
